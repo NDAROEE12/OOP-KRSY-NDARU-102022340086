d278cac452147aac47819d404aeaa387cd239e3e
NDAROEE12 <149312641+NDAROEE12@users.noreply.github.com>
2024-10-18 10:40:25 +0700

    Add files via upload

---
+++ MODUL1_NDARU/ManajemenInventaris.java
@@ -0,0 +1,50 @@
+// Don't delete any comments below!!!
+// Todo : Import Arraylist and Scanner
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class ManajemenInventaris {
+    // Todo : Create ArrayList of MakananKering (daftarMakananKering) and MakananBasah (daftarMakananBasah) to store items
+	
+	private ArrayList<MakananKering> daftarMakananKering = new ArrayList<>();
+    private ArrayList<MakananBasah> daftarMakananBasah = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+
+    public void tambahMakananKering()
+    
+    
+    {
+        
+        // Todo : Create input for Nama Makanan, Jumlah Makanan, Harga Makanan, and Brand Makanan
+    
+    	
+
+        // Todo : Create a new object for MakananKering
+        
+        // Todo : Create Print Notifitaction "Makanan kering berhasil ditambahkan"
+        
+    }
+
+    public void tambahMakananBasah() {
+        // Todo : Create input for Nama Makanan, Jumlah Makanan, Harga Makanan, and Bahan Makanan
+
+        // Todo : Create a new object for MakananBasah
+        
+        // Todo :Create Print Notifitaction "Makanan Basah berhasil ditambahkan"
+    }
+
+    public void tampilkanSemuaMakanan() {
+        if (daftarMakananKering.isEmpty() && daftarMakananBasah.isEmpty()) {
+            // Todo : Create Print Notification "Tidak ada makanan disini"
+        } else {
+            // Todo : Create print notification for Makanan Kering  
+            for 
+            }
+            // Todo : Create print notification for  Makanan Basah
+            for 
+            }
+        }
+    }
+}
